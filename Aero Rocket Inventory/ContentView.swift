cd9ea93eabaac37519f00c6bb86b690c9a387d01
p6blov <pablo.vasquez5002@outlook.com>
2025-03-20 13:39:40 -0400

    initial commit

--- Aero Rocket Inventory/ContentView.swift
+++ Aero Rocket Inventory/ContentView.swift
@@ -9,10 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                Color(.black)
+                    .ignoresSafeArea()
+                VStack {
+                    Text("SEDS Aerospace Rocket Inventory")
+                        .foregroundStyle(.white)
+                        .font(.largeTitle)
+                }
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    Spacer()
+                    VStack {
+                        Button(action: {
+                            print("Search tapped")
+                        }) {
+                            Image(systemName: "magnifyingglass")
+                                .font(.system(size: 20)) // Increase icon size
+                        }
+                        Text("Search").font(.caption).foregroundColor(.white)
+                    }
+                    Spacer()
+                    Spacer()
+                    VStack {
+                        Button(action: {
+                            print("Scan tapped")
+                        }) {
+                            Image(systemName: "qrcode.viewfinder")
+                                .font(.system(size: 20))
+                                .foregroundColor(.gray);
+                        }
+                        Text("Scan").font(.caption).foregroundColor(.gray)
+                    }
+                    Spacer()
+                    Spacer()
+                    VStack {
+                        Button(action: {
+                            print("Find tapped")
+                        }) {
+                            Image(systemName: "shippingbox")
+                                .font(.system(size: 20))
+                                .foregroundColor(.gray);
+                        }
+                        Text("Find").font(.caption).foregroundColor(.gray)
+                    }
+                    Spacer()
+                }
+            }
         }
-        .padding()
     }
 }
 
